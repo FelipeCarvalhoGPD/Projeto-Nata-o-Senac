945105a0162c0e0b2324e18c01ffa1c14b0499f1
Felipe Giovane Pereira de Carvalho <1115221@senacminas.edu.br>
2026-06-23 16:23:08 -0300

    v1.0

---
+++ backend/server.js
@@ -0,0 +1,159 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+const app = express();
+app.use(cors());
+app.use(express.json());
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "natacao"
+});
+db.connect((erro) => {
+    if(erro){
+        console.log("Erro ao conectar");
+        console.log(erro);
+        return;
+    }
+    console.log("Conectado com sucesso");
+})
+
+app.get("/", (req, res) => {
+    res.json({
+        mensagem: "API Funcionando."
+    })
+})
+
+app.post("/alunos", (req, res) => {
+    const{
+        nome, idade, telefone, nivel, horario
+    } = req.body
+        if(!nome || !idade || !telefone || !nivel || !horario){
+            return res.status(400).json({
+                erro: "Preencha todos os campos."
+            })
+        }
+        if(idade < 5){
+            return res.status(400).json({
+                 erro: "O aluno deve ter 5 anos ou mais."
+            })
+        }
+        if(idade > 100){
+            return res.status(400).json({
+                 erro: "O aluno deve ter menos de 100 anos."
+            })
+        }
+        if(nome.length < 3){
+            return res.status(400).json({
+                 erro: "O nome deve possuir mais de 3 caracteres."
+            })
+        }
+        
+        const verificaSQL = "select * from alunos where nome = ?";
+        db.query(verificaSQL, [nome], (erro, resultado) =>{
+            if(erro){
+                return res.status(500).json(erro);
+            }
+            if(resultado.legth > 0){
+                return res.status(400).json({
+                    erro: "Já existe este nome cadastrado no banco"
+                })
+            }
+            const inserirSQL = `insert into alunos (nome, idade, telefone, nivel, horario) values(?, ?, ?, ?, ?)`;
+            db.query(inserirSQL, [nome, idade, telefone, nivel, horario], (erro, resultado) => {
+                if(erro){
+                    return res.status(500).json(erro);
+                }
+                res.status(201).json({
+                    mensagem: "Aluno cadastrado",
+                    id: resultado.insertId
+                });
+            });
+        });    
+});
+
+app.get("/alunos", (req, res) => {
+    db.query(
+        "select * from alunos", (erro, resultado) => {
+            if(erro){
+                return res.status(500).json(erro);
+            }
+            res.json(resultado);
+        });
+});
+
+app.delete("/alunos/:id", (req, res) => {
+    const id = req.params.id;
+    db.query("delete from alunos where id = ?", [id], (erro, resultado) => {
+        if(erro){
+            return res.status(500).json(erro);
+        } if (resultado.affectedRows === 0){
+            return res.status(404).json({
+                erro: "Aluno não encontrado"
+            })
+        }
+        res.json({
+            mensagem: "Aluno removido"
+        });
+    });
+});
+
+app.put("/alunos/:id", (req, res) => {
+    const id = req.params.id;
+    db.query("select ativo from alunos where id = ?", [id], (erro, resultado) =>{
+        if(erro){
+            return res.status(500).json(erro);
+        }
+        if(resultado.length === 0){
+            return res.status(404).json({
+                erro: "Aluno não encontrado"
+            });
+        }
+        const novoStatus = resultado[0].ativo ? 0:1;
+        db.query("update alunos set ativo = ? where id = ?", [novoStatus, id], (erro) =>{
+            if(erro){
+                return res.status(500).json(erro);
+            }
+            res.json({
+                mensagem: "Aluno atualizado"
+            });
+        });
+    });
+});
+
+let incorretas = 0;
+let bloqueado = false;
+
+app.post("/admin", (req, res) => {
+    const {senha} = req.body; 
+    if(bloqueado === true){
+        return res.status(403).json({
+            erro: "Tentativas excedidas."
+        })
+    } 
+        if(!senha){
+        return res.status(400).json({
+            erro: "Informe a senha."
+        })
+    }
+    if(senha === "admin123"){
+        incorretas = 0
+        return res.json({autenticado: true});
+    }
+    incorretas++;
+    if(incorretas >= 3){
+        bloqueado = true;
+        return res.status(403).json({
+            erro: "Sistema bloqueado."
+        })
+    }
+    return res.status(401).json({
+        erro: "Senha incorreta."
+    })
+})
+
+app.listen(3000, () => {
+    console.log("Servidor rodando em: ")
+    console.log("http://localhost:3000")
+})
